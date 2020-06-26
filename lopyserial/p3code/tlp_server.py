0f217021219ab8829ef355dd569aa065d60ea02d
pmanzoni <manzoni@ieee.org>
2020-06-26 11:21:03 +0200

    Improving feedback messages

--- lopyserial/p3code/tlp_server.py
+++ lopyserial/p3code/tlp_server.py
@@ -29,7 +29,7 @@ def loractp_send(addr, pload):
 def loractp_recv(rcvraddr):
     try:
         rcvd_data, addr = ctpc.recvit(rcvraddr)
-        debug_print("loractp_recv: got {} from {}".format(rcvd_data, addr))
+        debug_print("got {} from {}".format(rcvd_data, addr))
         return rcvd_data
     except Exception as e:
         print ("EXCEPTION when receiving ->", e)
@@ -46,7 +46,7 @@ def fromservertoloractp(sock):
                 # either 0 or end of data
                 break
         indata = b"".join(data)
-        debug_print('fromservertoloractp: received {!r}'.format(indata))
+        debug_print('received {!r} from connected server'.format(indata))
         return indata
 
 
@@ -60,47 +60,48 @@ if __name__ == "__main__":
     else:
         debug_print("failed connection from {} to me ({})".format(rcvraddr, myaddr))
 
-    # start handshake
-    # waiting remote server data in the format:
-    # {"IP": <IPaddr>, "port": <port>}  
-    rcvd_data = loractp_recv(rcvraddr)
-    debug_print("got {}".format(rcvd_data))
-    conndata = json.loads(rcvd_data)
-    debug_print("handshake data:", conndata["IP"], int(conndata["port"]))
-
-    # Connect the socket to the request IP and port
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (conndata["IP"], int(conndata["port"]))
-        debug_print('connecting to {} port {}'.format(*server_address))
-        sock.connect(server_address)
-        debug_print("CONNECTED!")
-        # loractp_send(rcvraddr, b"CONNECTED\n")
-    except Exception as e:
-        debug_print("Exception connecting to requested server!", e)
-        debug_print("FAILED")
-        # loractp_send(rcvraddr, b"FAILED\n")
-    # end handshake
-
-    # handling data 
     while True:
+        # start handshake
+        # waiting remote server data in the format:
+        # {"IP": <IPaddr>, "port": <port>}  
         rcvd_data = loractp_recv(rcvraddr)
-        if CLOSECONN in rcvd_data:
-            print ("Closing connection...")
-            sock.close()
-            break
-        try: 
-            sock.sendall(rcvd_data)
-        except BrokenPipeError:
-            print("BrokenPipeError")
-            sock.close()
-            break
+        debug_print("got {}".format(rcvd_data))
+        conndata = json.loads(rcvd_data)
+        debug_print("handshake data:", conndata["IP"], int(conndata["port"]))
+
+        # Connect the socket to the request IP and port
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (conndata["IP"], int(conndata["port"]))
+            debug_print('connecting to {} port {}'.format(*server_address))
+            sock.connect(server_address)
+            debug_print("CONNECTED!")
+            # loractp_send(rcvraddr, b"CONNECTED\n")
         except Exception as e:
-            print ("Exception handling in/out data...", e)
-            sock.close()
-            break
-        indata = fromservertoloractp(sock)
-        loractp_send(rcvraddr, indata)
+            debug_print("Exception connecting to requested server!", e)
+            debug_print("FAILED")
+            # loractp_send(rcvraddr, b"FAILED\n")
+        # end handshake
+
+        # handling data 
+        while True:
+            rcvd_data = loractp_recv(rcvraddr)
+            if CLOSECONN in rcvd_data:
+                print ("Closing connection...")
+                sock.close()
+                break
+            try: 
+                sock.sendall(rcvd_data)
+            except BrokenPipeError:
+                print("BrokenPipeError")
+                sock.close()
+                break
+            except Exception as e:
+                print ("Exception handling in/out data...", e)
+                sock.close()
+                break
+            indata = fromservertoloractp(sock)
+            loractp_send(rcvraddr, indata)
 
 
 
